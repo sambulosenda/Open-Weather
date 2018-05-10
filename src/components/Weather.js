f004da6bb10cd78fa0ba7c282fdd3d8d464f0dda
sambulo <sambulosendas@gmail.com>
2018-05-10 18:52:44 +0100

    udffadfasdbsdbasbXXX

---
+++ src/components/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Weather extends React.Component {
+    render(){
+        return(
+            <div>
+            <p>Weather Component</p>
+
+
+            </div>
+        )
+    }
+}
+
+export default Weather;
